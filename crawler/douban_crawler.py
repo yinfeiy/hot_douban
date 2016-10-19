13d085210e2e801c04f0648253cf72a02d30188f
Yinfei Yang <yangyin7@gmail.com>
2016-10-18 19:54:32 -0700

    add hearders to request

--- crawler/douban_crawler.py
+++ crawler/douban_crawler.py
@@ -1,14 +1,16 @@
-from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.http import Request
 import simplejson as json
+from items import DoubanMovieItem
 
 class DoubanCrawler(CrawlSpider):
     name = "douban"
-    #allowed_domains = ["movie.douban.com"]
-    #start_urls = ["http://movie.douban.com/top250"]
-    allowed_domains = ["fling.seas.upenn.edu/"]
-    start_urls = ["http://movie.douban.com/~yinfeiy/"]
+    allowed_domains = ["movie.douban.com"]
+    start_urls = ["https://movie.douban.com/top250"]
+    #allowed_domains = ["fling.seas.upenn.edu/"]
+    #start_urls = ["https://fling.seas.upenn.edu/~yinfeiy/"]
 
     rules = (
         Rule(
@@ -20,14 +22,15 @@ class DoubanCrawler(CrawlSpider):
             callback = 'parse_page', follow = True)
         )
 
+    def start_requests(self):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'}
+        for i,url in enumerate(self.start_urls):
+            yield Request(url,cookies={'over18':'1'}, callback=self.parse_page, headers=headers)
+
     def parse_page(self, response) :
         sel = Selector(response)
-        item = dict()
+        item = DoubanMovieItem()
         item['name'] = sel.xpath('//h1/span[@property="v:itemreviewed"]/text()').extract()
-        item['description'] = sel.xpath('//div/span[@property="v:summary"]/text()').extract()
+        item['desc'] = sel.xpath('//div/span[@property="v:summary"]/text()').extract()
         item['url'] = response.url
-        print json.dumps(item)
         return item
-
-if __name__ == '__main__':
-    DoubanCrawler()
